4f6d74676dafad6df37c8c05b2a87625617add38
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-06-03 01:04:16 +0300

    test

---
+++ src/components/LatestTwitts/LatestTwitts.jsx
@@ -0,0 +1,9 @@
+import { Container } from 'components/App.styled';
+
+export const LatestTwitts = () => {
+    return (
+        <Container>
+            <div>Latest twitts</div>
+        </Container>
+    );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import { Section } from 'components/App.styled';
 import { Hero } from 'components/Hero/Hero';
+import { LatestTwitts } from 'components/LatestTwitts/LatestTwitts';
 
 const Home = () => {
     return (
         <Section>
             <Hero />
+            <LatestTwitts />
         </Section>
     );
 };
