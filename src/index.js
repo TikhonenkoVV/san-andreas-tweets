472e78fcdb7815a997c4606a606c22c4c5c7b697
TikhonenkoVV <113926273+TikhonenkoVV@users.noreply.github.com>
2023-06-03 15:58:02 +0300

    fix browser basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     // <React.StrictMode>
-    <BrowserRouter basename="/san-andreas-twitts">
+    <BrowserRouter basename="/san-andreas-tweets">
         <ThemeProvider theme={theme}>
             <Global styles={GlobalStyles} />
             <App />
